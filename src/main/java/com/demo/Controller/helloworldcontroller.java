52a5f7db3722c5973fc06d287a1471f1b7df2f2c
pradeep yadav <pradeepyadav211209@gmail.com>
2026-02-04 14:15:48 +0530

    demo-app v1

---
+++ src/main/java/com/demo/Controller/helloworldcontroller.java
@@ -0,0 +1,13 @@
+package com.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloworldcontroller {
+ 
+	@GetMapping("/Message")
+	public String getMessage() {
+		return "Hello-world";
+	}
+}
